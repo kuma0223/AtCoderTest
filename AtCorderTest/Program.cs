14b40be7c022eda9637585b0bba09dcd506bfad9
kuma0223 <kabocha@yowano0411.sakura.ne.jp>
2020-06-13 10:06:21 +0900

    initialize

---
+++ AtCorderTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    const string Yes = "Yes";
+    const string No = "No";
+    const char White = '.';
+    const char Black = '#';
+    const long Mod = 1000000007;
+
+    static void Main(string[] args) {
+        checked {
+#if DEBUG
+            MyDebugger.Test(process, "../TextFile1.txt");
+#else
+            object ret = process(new StreamReader(Console.OpenStandardInput()));
+            Console.WriteLine(ret);
+#endif
+        }
+    }
+
+    static object process(TextReader input) {
+        var n = int.Parse(input.ReadLine());
+        var ary = ToIntArray(input.ReadLine(), n);
+
+        return null;
+    }
+
+    static int[] ToIntArray(string strs, int n) {
+        var ret = new int[n];
+        var spl = strs.Split(' ');
+        for (int i = 0; i < n; i++)
+            ret[i] = int.Parse(spl[i]);
+        return ret;
+    }
+}
---
+++ AtCorderTest/TestCaseMaker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class TestCaseMaker
+{
+    public static void MakeTestCase1(string inputfile) {
+        Random r = new Random();
+
+        using (var writer = new StreamWriter(inputfile)) {
+            writer.WriteLine(2*100000);
+            for(int i=0; i<2*100000; i++) {
+                writer.WriteLine("1 1000000000");
+            }
+        }
+    }
+}
